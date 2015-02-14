bef748eab3b04de68ef5d36ca581cd6c3b928a90
Marc CARRE <carre.marc@gmail.com>
2015-02-14 10:46:38 +0000

    Now using the 'six' package for compatibility with both Python 2.* and 3.*.

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,9 +1,9 @@
 import sys
-from io import BytesIO
+from six import StringIO
 
 class FakeStdOut(object):
     def __enter__(self):
-        self._stream = BytesIO()
+        self._stream = StringIO()
         self._original_stdout, sys.stdout = sys.stdout, self._stream
         return self._stream
     def __exit__(self, type, value, traceback):
@@ -12,7 +12,7 @@ class FakeStdOut(object):
 
 class FakeStdErr(object):
     def __enter__(self):
-        self._stream = BytesIO()
+        self._stream = StringIO()
         self._original_stderr, sys.stderr = sys.stderr, self._stream
         return self._stream
     def __exit__(self, type, value, traceback):
