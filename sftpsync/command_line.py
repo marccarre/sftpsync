33cea811d80c2a94685bb04400797be2084c0abf
Marc CARRE <carre.marc@gmail.com>
2015-02-14 10:20:40 +0000

    Now using BytesIO instead of StringIO, and more Pythonic/Python3-friendly way to concatenate usage strings.

--- sftpsync/command_line.py
+++ sftpsync/command_line.py
@@ -3,36 +3,37 @@ import sys
 
 def usage(error_message=None):
     if error_message:
-        sys.stderr.write(u'ERROR: ' + error_message + linesep)
+        sys.stderr.write('ERROR: ' + error_message + linesep)
 
-    sys.stdout.write(
-        u'Usage:' + linesep + \
-        '    sftpsync.py [OPTION]... SOURCE DESTINATION' + linesep + \
-        'Pull:' + linesep + \
-        '    sftpsync.py [OPTION]... [user[:password]@]host[:[port]/path] /path/to/local/copy' + linesep + \
-        'Push:' + linesep + \
-        '    sftpsync.py [OPTION]... /path/to/local/copy [user[:password]@]host[:[port]/path]' + linesep + linesep + \
-
-        'Defaults:' + linesep + \
-        '    user:     anonymous' + linesep + \
-        '    password: anonymous' + linesep + \
-        '    port:     22' + linesep + \
-        '    path:     /' + linesep + linesep + \
-
-        'Options:' + linesep + \
-        '-f/--force      Force the synchronization regardless of files\' presence or timestamps.' + linesep + \
-        '-h/--help       Prints this!' + linesep + \
-        '-i/--identity identity_file' + linesep + \
-        '                Selects the file from which the identity (private key) for public key authentication is read.' + linesep + \
-        '-o ssh_option' + linesep + \
-        '                Can be used to pass options to ssh in the format used in ssh_config(5). This is useful for specifying options for which there is no separate sftpsync command-line flag. For full details of the options listed below, and their possible values, see ssh_config(5).' + linesep + \
-        '                    ProxyCommand' + linesep + \
-        '-p/--preserve:  Preserves modification times, access times, and modes from the original file.' + linesep + \
-        '--proxy [user[:password]@]host[:port]' + linesep + \
-        '                SOCKS proxy to use. If not provided, port will be defaulted to 1080.' + linesep + \
-        '--proxy-version SOCKS4|SOCKS5' + linesep + \
-        '                Version of the SOCKS protocol to use. Default is SOCKS5.' + linesep + \
-        '-q/--quiet:     Quiet mode: disables the progress meter as well as warning and diagnostic messages from ssh(1).' + linesep + \
-        '-r/--recursive: Recursively synchronize entire directories.' + linesep + \
-        '-v/--verbose:   Verbose mode. Causes sftpsync to print debugging messages about their progress. This is helpful in debugging connection, authentication, and configuration problems.' + linesep + linesep
-    )
+    sys.stdout.write(linesep.join([
+        'Usage:',
+        '    sftpsync.py [OPTION]... SOURCE DESTINATION',
+        'Pull:',
+        '    sftpsync.py [OPTION]... [user[:password]@]host[:[port]/path] /path/to/local/copy',
+        'Push:',
+        '    sftpsync.py [OPTION]... /path/to/local/copy [user[:password]@]host[:[port]/path]',
+        linesep,
+        'Defaults:',
+        '    user:     anonymous',
+        '    password: anonymous',
+        '    port:     22',
+        '    path:     /',
+        linesep,
+        'Options:',
+        '-f/--force      Force the synchronization regardless of files\' presence or timestamps.',
+        '-h/--help       Prints this!',
+        '-i/--identity identity_file',
+        '                Selects the file from which the identity (private key) for public key authentication is read.',
+        '-o ssh_option',
+        '                Can be used to pass options to ssh in the format used in ssh_config(5). This is useful for specifying options for which there is no separate sftpsync command-line flag. For full details of the options listed below, and their possible values, see ssh_config(5).',
+        '                    ProxyCommand',
+        '-p/--preserve:  Preserves modification times, access times, and modes from the original file.',
+        '--proxy [user[:password]@]host[:port]',
+        '                SOCKS proxy to use. If not provided, port will be defaulted to 1080.',
+        '--proxy-version SOCKS4|SOCKS5',
+        '                Version of the SOCKS protocol to use. Default is SOCKS5.',
+        '-q/--quiet:     Quiet mode: disables the progress meter as well as warning and diagnostic messages from ssh(1).',
+        '-r/--recursive: Recursively synchronize entire directories.',
+        '-v/--verbose:   Verbose mode. Causes sftpsync to print debugging messages about their progress. This is helpful in debugging connection, authentication, and configuration problems.',
+        linesep
+    ]))
